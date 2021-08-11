b2361ccedfea4bd794538f3275ef0541349d98d5
Dayem Siddiqui <dayemsiddiqui@gmail.com>
2021-08-11 19:14:50 +0200

    add a hello component

---
+++ src/Hello.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const Hello: React.FC<{}> = ({}) => <div>Hello From the Smart Component</div>
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export * from './exampleModule'
+export * from './Hello';
